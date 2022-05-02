ef87a63f24e6f485287b3e10c53ec191d5d48b79
Stephen <hellostephenwm@gmail.com>
2022-05-02 17:21:25 -0400

    Configured colors

--- src/config/index.js
+++ src/config/index.js
@@ -2,12 +2,15 @@
 const path = require('path')
 // Importing dotenv
 const dotenv = require('dotenv')
+// Importing colors
+const colors = require('colors')
 
 // Configuring location of env variables
 dotenv.config({ path: path.resolve(__dirname, '../.env') })
 
 // Exporting configurations
 module.exports = {
-    'database_name': 'db.sqlite3',
-    'port': process.env.PORT || 5000
+    'database_name': './src/db/db.sqlite3',
+    'port': process.env.PORT || 5000,
+    colors
 }
