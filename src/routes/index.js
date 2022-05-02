b2b4cb8db1a74fa51aedda38fc48c02c0d4f8df7
Stephen <hellostephenwm@gmail.com>
2022-05-02 15:09:49 -0400

    Router for root endpoint

---
+++ src/routes/index.js
@@ -0,0 +1,31 @@
+// Importing colors
+const colors = require('colors')
+// Importing express router
+const router = require('express').Router()
+
+// Enabling colors
+colors.enable()
+
+/**
+ * @params NONE
+ * @method GET
+ * This is a test GET request of our root API URL, if successful,
+ * the user will be met with a message:OK to indicate that the API is up and running.
+ * If unsuccesful, the user will be met with an error message.
+ */
+router.get('/', (req, res) => {
+    try {
+        return res.status(200).json({
+            message: 'OK'
+        })
+    } catch(error) {
+        console.log(error.message.red)
+
+        return res.status(500).json({
+            message: 'Something went wrong.'
+        })
+    }
+})
+
+// Exporting router
+module.exports = router
