343aa017a6d334f6814242d8a806272295a557ad
Stephen <hellostephenwm@gmail.com>
2022-05-02 21:57:35 -0400

    Configured db with extra columns

--- src/db/index.js
+++ src/db/index.js
@@ -30,6 +30,11 @@ let db = new sqlite3.Database(config.database_name, (err) => {
             year INT,
             make TEXT,
             model TEXT,
+            racer_turbo INT,racer_supercharged INT,racer_performance INT,racer_horsepower INT,
+            car_overall INT,
+            engine_modifications INT,engine_performance INT,engine_chrome INT,engine_detailing INT,engine_cleanliness INT,
+            body_frame_undercarriage INT,body_frame_suspension INT,body_frame_chrome INT,body_frame_detailing INT,body_frame_cleanliness INT,
+            mods_paint INT,mods_body INT,mods_wrap INT,mods_rims INT,mods_interior INT,mods_other INT,mods_ice INT,mods_aftermarket INT,mods_wip INT,mods_overall,
             score INT
             )`,
         (err) => {
@@ -38,7 +43,7 @@ let db = new sqlite3.Database(config.database_name, (err) => {
             } else {
                 csvtojson().fromFile(DATA_CSV)
                 .then(data => {
-                    let insert = 'INSERT INTO cars (Car_ID, Email, Name, Year, Make, Model, Score) VALUES (?,?,?,?,?,?,?)';
+                    let insert = 'INSERT INTO cars (Car_ID, Email, Name, Year, Make, Model, Racer_Turbo, Racer_Supercharged, Racer_Performance, Racer_Horsepower, Car_Overall, Engine_Modifications, Engine_Performance, Engine_Chrome, Engine_Detailing, Engine_Cleanliness, Body_Frame_Undercarriage, Body_Frame_Suspension, Body_Frame_Chrome, Body_Frame_Detailing, Body_Frame_Cleanliness, Mods_Paint, Mods_Body, Mods_Wrap, Mods_Rims, Mods_Interior, Mods_Other, Mods_ICE, Mods_Aftermarket, Mods_WIP, Mods_Overall, Score) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)';
 
                     for(const item of data) {
                         let total = parseInt(item.Racer_Turbo) + parseInt(item.Racer_Supercharged) + parseInt(item.Racer_Performance) + parseInt(item.Racer_Horsepower) + parseInt(item.Car_Overall) + parseInt(item.Engine_Modifications) + parseInt(item.Engine_Performance) + parseInt(item.Engine_Chrome) + parseInt(item.Engine_Detailing) + parseInt(item.Engine_Cleanliness) + parseInt(item.Body_Frame_Undercarriage) + parseInt(item.Body_Frame_Suspension) + parseInt(item.Body_Frame_Chrome) + parseInt(item.Body_Frame_Detailing) + parseInt(item.Body_Frame_Cleanliness) + parseInt(item.Mods_Paint) + parseInt(item.Mods_Body) + parseInt(item.Mods_Wrap) + parseInt(item.Mods_Rims) + parseInt(item.Mods_Interior) + parseInt(item.Mods_Other) + parseInt(item.Mods_ICE) + parseInt(item.Mods_Aftermarket) + parseInt(item.Mods_WIP) + parseInt(item.Mods_Overall);
@@ -46,10 +51,14 @@ let db = new sqlite3.Database(config.database_name, (err) => {
                         db.run(insert, [
                             item.Car_ID,
                             item.Email,
-                            item.Name, 
+                            item.Name,
                             item.Year,
-                            item.Make, 
+                            item.Make,
                             item.Model,
+                            item.Racer_Turbo,item.Racer_Supercharged,item.Racer_Performance,item.Racer_Horsepower,item.Car_Overall,
+                            item.Engine_Modifications,item.Engine_Performance,item.Engine_Chrome,item.Engine_Detailing,item.Engine_Cleanliness,
+                            item.Body_Frame_Undercarriage,item.Body_Frame_Suspension,item.Body_Frame_Chrome,item.Body_Frame_Detailing,item.Body_Frame_Cleanliness,
+                            item.Mods_Paint,item.Mods_Body,item.Mods_Wrap,item.Mods_Rims,item.Mods_Interior,item.Mods_Other,item.Mods_ICE,item.Mods_Aftermarket,item.Mods_WIP,item.Mods_Overall,
                             total
                         ]);
                     }
